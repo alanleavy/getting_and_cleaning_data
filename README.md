07e36b70a167b373b9b4262fcf1dbe532e0e2a46
alanleavy <alan_leavy@gmx.com>
2014-06-16 16:04:01 +0100

    initial check-in

---
+++ README.md
@@ -0,0 +1,70 @@
+# Getting and Cleaning Data - Peer Assessment Project
+
+
+## Introduction
+
+This is the peer assessment project for the  "Getting and Cleaning Data" course.
+
+The components of this project are:
+* README.md - this file
+* CodeBook.md - a code book describesing the data, its variables any transformations that were performed to clean up the data.
+* run_analysis.R - An R script file containing all functions that were used to clean the data set. This is described in more detail below
+ 
+## run_analysis.R
+
+### Instructions for running the data cleaning script
+1. _Set the appropriate working directory_   
+The data cleaning functions in run_analysis.R assume that the UCI HAR dataset has already been downloaded and unzipped into the current working directory, appearing as a sub-directory called 'UCI HAR Dataset'.  
+If this is not the case then the **download_dataset()** function may be called. This function:
+    * downloads the dataset from https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip,
+    * unzips into a timestamped directory under the current working directory (e.g. "UCI_HAR_Dataset__2014-06-16") and 
+    * makes this new directory the current working directory.  - The unzipped 'UCI HAR Dataset' directory is now within the current working directory and data cleaning functions can now be run.  
+
+2. _Run the data cleaning functions_  
+The main entry point for the data cleaning script is the function **clean_data()** which implelements the end-to-end process specified  for the project:
+> You should create one R script called run_analysis.R that does the following.   
+> 1. Merges the training and the test sets to create one data set.  
+> 2. Extracts only the measurements on the mean and standard deviation for each measurement.   
+> 3. Uses descriptive activity names to name the activities in the data set  
+> 4. Appropriately labels the data set with descriptive variable names.   
+> 5. Creates a second, independent tidy data set with the average of each variable for each activity and each subject  
+
+  **clean_data()** takes no parameters and creates two files in the current working directory:
+    * **tidy_data_1.txt** - the output of steps 1-4 above
+    * **tidy_data_2.txt** - the output of step 5 above
+    
+### Overview of the implementation of clean_data()
+**clean_data()** performs the following steps:
+1. Merge the various components of the training and test datasets  
+this is performed by 3 sub functions:  
+  * **merge_features()** - reads "UCI HAR Dataset/train/X_train.txt" and "UCI HAR Dataset/test/X_test.txt", and concatenates them into a single dataframe which is returned to the calling function.  
+  * **merge_activities()** - reads "UCI HAR Dataset/train/y_train.txt" and "UCI HAR Dataset/test/y_test.txt", and concatenates them into a single dataframe which is returned to the calling function.  
+  * **merge_subjects()** - reads "UCI HAR Dataset/train/subject_train.txt" and "UCI HAR Dataset/test/subject_test.txt", and concatenates them into a single dataframe which is returned to the calling function.  
+
+2. Filter for just the features we're interested in (averages and standard deviations)  
+This is done by the function **select_features()**, which:
+  * reads in in the complete set of original feature names and column numbers from "UCI HAR Dataset/features.txt"
+  * identifies which feature labels contain either "-std()" OR "-mean()
+  * returns a dataframe with two columns: V1 - the index of a features in the original dataset; V2 - the corresponsing feature label from the original dataset. This data frame has one row for each of the features identified in the previous step.  
+2. **clean _data()** uses the first column of this returned dataframe subset the dataframe returned previously by **merge_features()**, retaining just the required features.
+3. Create descriptive feature names, cleaned versions of the UCI HAR feature labels, and add them to our filtered data frame  
+**clean _data()** calls **modify_feature_names()** to do this, passing the dataframe previously returned by **select_features()**. The new feature names are based on the original ones in "UCI HAR Dataset/features.txt", but **modify_feature_names()** modifies them to facilitate their usage for analysis within R, by:  
+  * removing "()" to avoid possible confusion with fucntion calls
+  * replacing "-" with "." to avoid possible confusion with subtraction
+  * converting camel case to underscore, for ease of reading (e.g. fBodyBodyGyroJerkMag_std Vs f_body_body_gyro_jerk_mag_std)
+  * replacing  the leading "t_" with "time."
+  * replacing the leading "f_" with "freq."
+  * adding a column (**$new_names**) with these modified names to the dataframe provided as input, and returning it to the calling function.
+3. **clean _data()** then takes the new column from the dataframe returned by **modify_feature_names()** new column and uses it to populate the column names of the previously filtered and merged merged data set
+4. Add an 'activity' column containing a descriptive activity name for each measurement.  
+The original UCI HAR activity labels are fit for purpose as they are.  
+**clean_data()** does this by calling **create_activity_column()**, passing the data frame returned by **merge_activities()** in step 1 above.  
+**create_activity_column()**:
+  * reads the  original UCI HAR activity labels from "UCI HAR Dataset/activity_labels.txt"
+  * looks up the activity value in each row of the merged activity dataframe (which was pased as a parameter) and gets the corrresponsing activity label
+  * returns a character vector with the corresponding activity label for each row of the merged dataset
+3. **clean _data()** then adds this returned vector filtered and merged data set as a new column called **$activity**
+5. Add a 'subject_id' column identifying the subject who performed the activity for each  measurement.  
+**clean_data()** does this simply by adding the first column of the dataframe returned by **merge_subjects()** in step 1 to the filtered and merged data set. the new column name is **$subject_id**
+6. Write out the first tidy dataset
+7. Create and write out the second tidy dataset
---
+++ run_analysis.R
@@ -0,0 +1,170 @@
+download_dataset <- function(){
+  ## Download in the UCI HAR dataset, unzip it into a new timestamped directory and make that the working directory
+  
+  fileURL <- 'https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip'
+  download.file(fileURL, "UCI_HAR_Dataset.zip")
+  destination_folder <- paste("UCI_HAR_Dataset", Sys.Date(), sep="__")
+  unzip("UCI_HAR_Dataset.zip", exdir = destination_folder, setTimes = T)
+  setwd(destination_folder)
+}
+
+clean_data <- function(){
+  ## This is the  main function for cleaning the UCI HAR Dataset  
+  ## Assumes that the original UCI data is in a folder "UCI HAR Dataset", in the current working directory
+  
+    ## Merge the various components of the training and test datasets
+  df_tidy_data <- merge_features()
+  df_merged_activities <- merge_activities()
+  df_merged_subjects <- merge_subjects()
+  
+    ## Filter for just the features we're interested in (averages and standard deviations)
+  df_relevant_features <- select_features()
+  df_tidy_data <- df_tidy_data[,df_relevant_features[,1]]
+  
+    ## Create descriptive feature names, cleaned versions of the UCI HAR feature labels,
+    ##  and add them to our filtered data frame
+  df_relevant_features <- modify_feature_names(df_relevant_features)
+  colnames(df_tidy_data) <- df_relevant_features$new_names
+
+    ## Add an 'activity' column containing a descriptive activity name for each measurement.
+    ## The original UCI HAR activity labels are fit for purpose as they are.
+  df_tidy_data$activity <- create_activity_column(df_merged_activities)  
+  
+    ## Add a 'subject_id' column identifying the subject who performed the activity for each
+    ##  measurement. 
+  df_tidy_data$subject_id <- df_merged_subjects$V1 
+  
+    ## Write out the first tidy dataset
+  write.table(df_tidy_data, "tidy_data_1.txt", row.names = F, sep="\t")
+  
+    ## Create and write out the second tidy dataset
+  df_tidy_data_2 <- create_summary_dataframe(df_tidy_data)  
+  write.table(df_tidy_data_2, "tidy_data_2.txt", row.names = F, sep="\t")
+}
+
+
+merge_features <- function(){
+  
+  ## Assumes that the original UCI data is in a folder "UCI HAR Dataset", in the current working directory
+  
+    ## Read the training measurement set into a dataframe
+  h_X_train <- file("UCI HAR Dataset/train/X_train.txt")
+  df_X_train <- read.table(h_X_train)
+  
+    ## Read the testing measurement set into a dataframe
+  h_X_test <- file("UCI HAR Dataset/test/X_test.txt")
+  df_X_test <- read.table(h_X_test)
+  
+    ## Return a merged dataframe
+  rbind(df_X_train,df_X_test)
+}
+
+
+merge_activities <- function(){
+  
+  ## Assumes that the original UCI data is in a folder "UCI HAR Dataset", in the current working directory
+  
+  ## Read the training activity data into a dataframe
+  h_y_train <- file("UCI HAR Dataset/train/y_train.txt")
+  df_y_train <- read.table(h_y_train)
+  
+  ## Read the testing activity data  into a dataframe
+  h_y_test <- file("UCI HAR Dataset/test/y_test.txt")
+  df_y_test <- read.table(h_y_test)
+  
+  ## Return a merged dataframe
+  rbind(df_y_train,df_y_test)
+}
+
+
+merge_subjects <- function(){
+  
+  ## Assumes that the original UCI data is in a folder "UCI HAR Dataset", in the current working directory
+  
+  ## Read the training subject data into a dataframe
+  h_subject_train <- file("UCI HAR Dataset/train/subject_train.txt")
+  df_subject_train <- read.table(h_subject_train)
+  
+  ## Read the testing subject data into a dataframe
+  h_subject_test <- file("UCI HAR Dataset/test/subject_test.txt")
+  df_subject_test <- read.table(h_subject_test)
+  
+  ## Return a merged dataframe
+  rbind(df_subject_train,df_subject_test)
+}
+
+
+select_features <- function(){
+  
+  
+  ## Assumes that the original UCI data is in a folder "UCI HAR Dataset", in the current working directory
+  
+    ## Read in the complete set of original feature names and column numbers
+  h_feature <- file("UCI HAR Dataset/features.txt")
+  df_feature <- read.table(h_feature)
+  
+    ## Identify which features / columns contain either "-std()" OR "-mean()"
+  lv_select_features <- grepl("-std\\(\\)|mean\\(\\)",df_feature$V2)
+  
+   ## Sub-set only those features and return a modified feature name / column number dataframe
+  df_feature[lv_select_features,]
+}
+
+
+modify_feature_names <- function(df_features){
+  ## This function takes the original UCI feature labels and modifies them to facilitate subsequqnt analysis using R
+  ## The input parameter is teh selected feature dataframe previously returned by select_features()
+  
+  
+    ## Remove "()" to avoid possible confusion with fucntion calls in R
+  new_names <- gsub("\\(\\)","",df_features$V2)
+  
+    ## Replace "-" with "." to avoid possible confusion with subtraction in R
+  new_names <- gsub("-",".",new_names)
+  
+    ## Convert camel case to underscore, becasue I think it's easier to read 
+    ## (e.g. fBodyBodyGyroJerkMag_std Vs f_body_body_gyro_jerk_mag_std)
+  new_names <-  gsub("([A-Z])", "_\\L\\1", new_names, perl=T)
+    ## Tidy up side effect of converting things like '.X' to ._x
+  new_names <-  gsub("\\._", "\\.", new_names, perl=T)
+  
+    ## Replace the leading "t_" with "time."
+  new_names <-  gsub("^t_", "time.", new_names, perl=T)
+  
+    ## Replace the leading "f_" with "freq."
+  new_names <-  gsub("^f_", "frequency.", new_names, perl=T)
+  
+    ## Add the new names as a new column to the data frame passed in df_features, and return it.
+  df_features$new_names <- new_names
+  df_features
+}
+
+
+create_activity_column <- function(df_activity_numbers){
+  
+  ## Assumes that the original UCI data is in a folder "UCI HAR Dataset", in the current working directory
+  
+    ## Read the original activity labels  set into a dataframe
+  h_activity_labels <- file("UCI HAR Dataset/activity_labels.txt")
+  df_activity_labels <- read.table(h_activity_labels)
+  
+    ## Look up the activity numbers in the activity labels dataframe, and return a dataframe with the 
+    ##  corresponsing label for each element in df_activity_numbers
+  ## df_activity_labels[df_activity_numbers[,1],2]  
+  ## sapply(df_activity_numbers[1], function(x){df_activity_labels[x,2]}) 
+  sapply(df_activity_numbers$V1, function(x){df_activity_labels$V2[x]}) 
+}
+
+
+create_summary_dataframe <- function(df){
+    ## We're using the plyr package to summarise the data set
+  library(plyr)
+  
+    ## Create a new data frame with the average of each column, except $activity, grouped by $subject_id
+  new_df <- ddply(df[,c(1:66,68)],"subject_id", colwise(mean))
+  
+    ## Modify the column names of the new dataframe, except $subject_id, prefixing the original names with "average."
+  colnames(new_df)[2:67] <- paste("average", colnames(new_df)[2:67], sep=".")
+  new_df
+  
+}
